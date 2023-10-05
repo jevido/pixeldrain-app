a8012839fe41dde9c52103ffa1b7225e7594014f
jeffrey duivenvoorden <jeffrey.duivenvoorden@allunited.nl>
2023-10-05 14:59:37 +0200

    Buttons > buttons

--- src/routes/(authed)/settings/+page.svelte
+++ src/routes/(authed)/settings/+page.svelte
@@ -1,11 +1,11 @@
 <script>
+	import { Button } from '$lib/primitives';
 	import { preferences } from '$lib/stores';
-    console.debug($preferences)
 </script>
 
 Settings page
-<button on:click={() => ($preferences.theme = 'dark')}>dark</button>
-<button on:click={() => ($preferences.theme = 'funk')}>funk</button>
-<button on:click={() => ($preferences.theme = 'white')}>white</button>
+<Button on:click={() => ($preferences.theme = 'dark')}>dark</Button>
+<Button on:click={() => ($preferences.theme = 'funk')}>funk</Button>
+<Button on:click={() => ($preferences.theme = 'white')}>white</Button>
 
 theme: {$preferences.theme}
