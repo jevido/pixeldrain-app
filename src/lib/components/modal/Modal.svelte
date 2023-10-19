2e974f57d53c5da7032112139b9ca9e61c330fda
jeffrey duivenvoorden <jeffrey.duivenvoorden@allunited.nl>
2023-10-19 09:55:04 +0200

    drown the voices

--- src/lib/components/modal/Modal.svelte
+++ src/lib/components/modal/Modal.svelte
@@ -1,14 +1,16 @@
 <script>
 	export let modalShown = false;
+
+	function close() {
+		modalShown = false;
+	}
 </script>
 
 {#if modalShown}
 	<div
 		class="fixed top-0 left-0 h-screen w-screen bg-background/60 z-50 flex items-center rounded"
 		tabindex="-1"
-		on:click={() => {
-			modalShown = false;
-		}}
+		on:click={close}
 	>
 		<div class="relative w-full max-w-md max-h-full rounded-md mx-auto" on:click|stopPropagation>
 			<div class="relative bg-background rounded-lg shadow">
