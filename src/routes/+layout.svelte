d8f42bf46093ff88e11828e0d0e07abf912673be
= <=>
2023-11-22 11:24:13 +0100

    Rebrand to app instead of pwa

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,7 +18,7 @@
 <svelte:head>
 	<title>PixelDrain</title>
 	<meta name="theme-color" content="#1c064a" />
-	<meta name="description" content="PixelDrain pwa" />
+	<meta name="description" content="PixelDrain app" />
 </svelte:head>
 
 <div class="block min-h-screen bg-background text-foreground">
