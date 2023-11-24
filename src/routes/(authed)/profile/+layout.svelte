135a376bf9b4b42605aba91388d0b798b90227a1
= <=>
2023-11-24 15:49:21 +0100

    Account page will be added in the future when forms are standardized

--- src/routes/(authed)/profile/+layout.svelte
+++ src/routes/(authed)/profile/+layout.svelte
@@ -1,17 +1,3 @@
-<script>
-	import { page } from '$app/stores';
-	import { buttonVariants } from '$lib/components/ui/button';
-</script>
-
-<div class="mx-8 mb-auto mt-8">
+<div class="m-8">
 	<slot />
 </div>
-
-{#if $page.url.pathname !== '/profile/account'}
-	<a href="/profile/account" class="{buttonVariants({ variant: 'secondary' })} py-6"> Account </a>
-{/if}
-{#if $page.url.pathname !== '/profile/appearance' && $page.url.pathname !== '/profile'}
-	<a href="/profile/appearance" class="{buttonVariants({ variant: 'secondary' })} py-6">
-		Appearance
-	</a>
-{/if}
